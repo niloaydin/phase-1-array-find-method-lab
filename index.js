9a3c6d4d5f47b3423e2ca209f2aec64dc87eb2a2
niloaydin <nilay.aydn97@gmail.com>
2022-04-20 17:56:18 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+
+
+function superbowlWin(array) {
+    const obj =  array.find(element => {
+        if (element.result === "W") {
+            return element.year
+        } else {
+            return undefined
+        }
+    })
+    if (obj === undefined) {
+        return undefined
+    } else {
+        return obj.year
+    }
+
+}
